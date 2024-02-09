13142ba730acfda91040638b98e45d9c20490d7b
ipekyilmaz35 <ipekyi35@gmail.com>
2024-02-09 14:26:15 +0100

    change on the order of the 1 const for app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true })); 
 app.get('/', (req, res) => {
@@ -19,7 +20,6 @@ app.post('/submit-form', (req, res) => {
     res.redirect('/');
 });
 
-const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`The server is running on the http://localhost:${PORT}`);
 });
